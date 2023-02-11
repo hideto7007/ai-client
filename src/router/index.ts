7fee28aa47cbf43fcf5188620115b7ae21800e31
hideto tsuzuki <ma_kux@icloud.com>
2023-02-11 15:31:39 +0900

    ルート確認OK

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Auth from '../views/Auth.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,11 +9,13 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView
   },
   {
+    path: '/auth',
+    name: 'auth',
+    component: Auth
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
---
+++ src/views/Auth.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="auth">
+        <h1>物体検知評価可視化</h1>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+  import HelloWorld from '@/components/HelloWorld.vue'; 
+  
+  @Options({
+    components: {
+      HelloWorld,
+    },
+  })
+  export default class Auth extends Vue {}
+  </script>
